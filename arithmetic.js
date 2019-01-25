bfb59acb9bc92872d0966423d37b8c0df12f9360
Connor Frendt <cjacksonfx@yahoo.com>
2019-01-24 16:51:54 -0800

    added basic four arithmetic functions, working on squaring

---
+++ arithmetic.js
@@ -0,0 +1,26 @@
+function addTwoNumbers(x, y) {
+  return x + y;
+}
+
+
+function subtractTwoNumbers(x, y) {
+  return x - y;
+}
+
+function multiplyTwoNumbers(x, y) {
+  return x * y;
+}
+
+function divideTwoNumbers(x, y) {
+  return x / y;
+}
+
+function squareNumber(base, exponent) {
+  return base * base * base * base * ...;
+}
+
+console.log(squareNumber(3, 2));
+
+function orderOfOperations(f) {
+
+}
